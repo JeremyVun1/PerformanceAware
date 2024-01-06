bc204a8285b68bda04348aedc9b727c50bf27b23
jeremy vun <jeremy.vun@outlook.com>
2024-01-07 04:07:10 +1100

    gradient

--- 10_simulating_real_program/data/test.asm
+++ 10_simulating_real_program/data/test.asm
@@ -4,26 +4,28 @@ mov bp, word [0]
 
 ; cols, rows
 ; mov cx, 2
-mov dx, 100
+mov dx, 64
 
 ; rgb values
-mov ax, 65280
+mov ax, 64
+mov bx, 255
 
 mov si, 0
 
 col_loop:
-	mov cx, 100
+	mov cx, 64
 
 	row_loop:
 		mov word [bp], ax ; rgb
-		mov word [bp + 2], 255 ; alpha
-		;add ax, 10
-		;add bx, 10
+		mov word [bp + 2], bx ; alpha
 		add bp, 4
 
 		sub cx, 1
 		jnz row_loop
 
+	add bx, 256 ; +b
+	sub ax, 1   ; - g
+	add ax, 256 ; + r
 	sub dx, 1
 	jnz col_loop
 
